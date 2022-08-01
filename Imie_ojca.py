299c3e9e1ead3efb110e6d9e0dd4c8431677cf43
Kamila <morawskakamila@gmail.com>
2022-08-01 16:07:33 +0200

    dodanie pliku

---
+++ Imie_ojca.py
@@ -0,0 +1,15 @@
+# Program Imię twojego ojca bez samogłosek
+
+''' Program prezentuje to jak wygląda imie twojego ojca bez samogłosek'''
+
+imie = input("Podaj imię twojego ojca: ")
+informacja = ""
+samogloski = "aąeęiuoó"
+
+imie
+
+for litera in imie:
+    if litera not in samogloski:
+        informacja += litera
+
+print("Imię twojego ojca bez samogłosek to: ", informacja)
